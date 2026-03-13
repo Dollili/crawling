a2deb298f875b7bab0c01b3759628b260862e456
Doil <ad____da@naver.com>
2026-03-13 17:37:04 +0900

    fix

--- frontend/src/api/testApi.ts
+++
@@ -1,6 +0,0 @@
-import apiClient from './index'
-
-export const fetchTestMessage = async (): Promise<string> => {
-  const response = await apiClient.get<string>('/api/test')
-  return response.data
-}
